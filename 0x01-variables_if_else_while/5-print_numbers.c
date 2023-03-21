9330baada4cb0f0c604c8e1bbe8452c0f1951ec0
xolmasek005 <masekox005@gmail.com>
2023-03-20 20:43:35 -0700

    batch

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+int x = 'a';
+while (x <= 'z')
+{
+if (lw == 'e' || lw == 'q')
+{
+x += 1;
+}
+else
+{
+putchar(x);
+x += 1;
+}
+}
+putchar('\n');
+return (0);
+}
