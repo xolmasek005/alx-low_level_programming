57ccf399ccc6e9d5cc7b99f235b53a93e59544bd
xolmasek005 <masekox005@gmail.com>
2023-03-22 18:51:35 -0700

    fix putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,18 @@
 *main - prints the word
 *Return: 0 on success execution.
 */
+
 int main(void)
 {
 	char s[] = "_putchar";
 	int i = 0;
+	
 	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
 		i++;
 	}
 	_putchar(10);
+	
 	return (0);
 }
