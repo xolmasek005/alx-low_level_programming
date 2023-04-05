5673308720a43adfd6b20bb4da2a32ad104859b0
xolmasek005 <masekox005@gmail.com>
2023-04-04 18:31:40 -0700

    fix betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,16 +9,17 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    char *dest_ptr = dest;
-    char *src_ptr = src;
-
-    while (n > 0)
-    {
-        *dest_ptr = *src_ptr;
-        dest_ptr++;
-        src_ptr++;
-        n--;
-    }
-
-    return (dest);
+	char *dest_ptr = dest;
+	char *src_ptr = src;
+	
+	while (n > 0)
+	{
+        
+		*dest_ptr = *src_ptr;
+	 	dest_ptr++;
+		src_ptr++;
+		n--;
+	}
+	
+	return (dest);
 }
