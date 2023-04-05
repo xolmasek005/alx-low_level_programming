39f384383a40a2866a54e7694e0406e55ea7bf51
xolmasek005 <masekox005@gmail.com>
2023-04-04 18:18:18 -0700

    fix betty 2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return s;
-        s++;
-    }
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
 
-    if (*s == c)
-        return s;
-    else
-        return '\0';
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
 }
