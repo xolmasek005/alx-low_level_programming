2bb006382a0053cdbeb48fb771126e6ffec67d51
xolmasek005 <masekox005@gmail.com>
2023-04-04 17:35:15 -0700

    fix 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+    while (*s != '\0')
+    {
+        if (*s == c)
+            return s;
+        s++;
+    }
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+    if (*s == c)
+        return s;
+    else
+        return '\0';
 }
