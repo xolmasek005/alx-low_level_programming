a8c041508cfbba614129458007ce21252ab0a098
xolmasek005 <masekox005@gmail.com>
2023-06-08 00:42:43 -0700

    removed trailing white spaces

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,13 @@
 /*
  * _puts_recursion - recursive function
  * @s: input for string
- * 
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);	
+		_puts_recursion(s + 1);
 	}
 
 	else
