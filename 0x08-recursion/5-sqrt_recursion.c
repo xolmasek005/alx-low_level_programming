a82cf4f5185cbbf0fbd60adceae4d2034c26e73f
xolmasek005 <masekox005@gmail.com>
2023-06-13 05:48:39 -0700

    added sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
+int sqrt_recursion(int n, int x)
 int _sqrt_recursion(int n)
 {
-	
+	if (n <0 )
+	{
+		return (-1);
+	}
+	return sqrt_recursion(n,1);
+}
+
+int sqrt_recursion(int n, int x)
+{
+	if (x * x == n)
+	{
+		return x;
+	}
+	if (x * x > n)
+	{
+		return -1
+	}
+	return sqrt_recursion(n, x + 1);
 }
