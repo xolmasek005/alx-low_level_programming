31ca63502a3498524c7264d82a644cbbea81e2bf
xolmasek005 <masekox005@gmail.com>
2023-06-13 06:00:23 -0700

    added is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+int prime(int n, int x);
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime(n,n -1));
+}
+
+int prime(int n, int x)
+{
+	if (n % x == 0 && x > 0)
+	{
+		return (0);
+	}
+	if (x == 1)
+	{
+		return (1);
+	}
+	return (prime);
+}
