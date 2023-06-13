4141b71980e8270242b7c0bb1f500177ab4334c7
xolmasek005 <masekox005@gmail.com>
2023-06-13 05:51:51 -0700

    added missing colon

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int sqrt_recursion(int n, int x)
+int sqrt_recursion(int n, int x);
 int _sqrt_recursion(int n)
 {
 	if (n <0 )
@@ -18,7 +18,7 @@ int sqrt_recursion(int n, int x)
 	}
 	if (x * x > n)
 	{
-		return -1
+		return -1;
 	}
 	return sqrt_recursion(n, x + 1);
 }
